f5960a94714a5e325c61164601f7faaff8174f66
Marya Kaysina <linkinpark335@gmail.com>
2022-11-27 22:12:39 +0300

    updated#1 components pomodoro block

--- src/shared/PomodorBlock/PomodorBlock.tsx
+++ src/shared/PomodorBlock/PomodorBlock.tsx
@@ -4,13 +4,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import { authRequestAsync, IData } from '../../store/auth/actions';
 import { RootState } from '../../store/reducer';
 import { Button } from '../Button';
-import { DEFAULT_TIME_BREAK } from '../conts';
+import { DEFAULT_TIME_BREAK, DEFAULT_TIME_BREAK_LONG } from '../conts';
 import { Text, EColors } from '../Text';
 import styles from './pomodorblock.css';
 import { Timer } from './Timer';
 
 export function PomodorBlock() {
   const [time, setTime] = useState<number>(0);
+  const [count, setCount] = useState<number>(0);
   const [text, setText] = useState<string>('Введите название задачи');
   const [number, setNumber] = useState<string>('');
   const [classList, setClassList] = useState<string>('');
@@ -18,6 +19,7 @@ export function PomodorBlock() {
   const [isTimerPaused, setIsTimerPaused] = useState<boolean>(false);
   const [isAfterStart, setIsAfterStart] = useState<boolean>(false);
   const [isBtnStartActive, setIsBtnStartActive] = useState<boolean>(false);
+  const [isBreak, setIsBreak] = useState<boolean>(false);
 
   let startBtnText = 'Старт';
   if (isTimerActive) startBtnText = 'Пауза';
@@ -26,37 +28,106 @@ export function PomodorBlock() {
   let stopBtnText = isTimerPaused ? 'Стоп' : 'Сделано';
 
   const data = useSelector<RootState, IData[]>(state => state.auth.data);
-  const dispatch = useDispatch<any>();
-
   const currentAuth = data.sort((a, b) => b.logInDate - a.logInDate).slice(0, 1)[0].auth;
   const currentTasks = data.filter((item) => item.auth === currentAuth)[0].tasks;
   const current = currentTasks.filter((task) => !task.done).sort((a, b) => a.id - b.id)[0];
 
-  useEffect(() => {
-    if (current) {
-      setText(current.text);
-      setNumber(`${currentTasks.indexOf(current) + 1}`);
-      setTime(current.time);
-    } else {
-      setText('Введите название задачи');
-      setNumber('');
-      setTime(0);
-    }
-  }, [current]);
+  const dispatch = useDispatch<any>();
 
   useEffect(() => {
-    if (time > 0 && isTimerActive) {
-      setTimeout(setTime, 1000, time - 1);
-    } else if (time === 0) {
-      setIsTimerActive(false);
-      current.done = true;
-      dispatch(authRequestAsync(data));
-      setTime(DEFAULT_TIME_BREAK);
-      setIsTimerActive(true);
-    } else {
-      setIsTimerActive(false);
+    setText(current ? current.text : 'Введите название задачи');
+    setNumber(current ? `${currentTasks.indexOf(current) + 1}` : '');
+    setTime(current ? current.time : 0);
+
+    if (isBreak) {
+      setText('Перерыв');
+      setNumber('');
+      if (count < 4) {
+        setTime(DEFAULT_TIME_BREAK);
+      } else {
+        setTime(DEFAULT_TIME_BREAK_LONG);
+        setCount(0);
+      }
     }
-  }, [isTimerActive, time]);
+  }, [current, isBreak]);
+
+  useEffect(() => {
+    if (current || isBreak) {
+      if (isTimerActive) {
+        if (time > 0) {
+          const currentTimerId = setTimeout(() => {
+            setTime(time - 1);
+            clearTimeout(currentTimerId);
+          }, 1000);
+        } else {
+          if (!isBreak) {
+            current.done = true;
+            dispatch(authRequestAsync(data));
+            setCount(count + 1);
+          }
+          setIsTimerActive(false);
+          setIsBreak(!isBreak);
+        }
+      }
+    }
+  }, [isTimerActive, time])
+
+  // useEffect(() => {
+  //   const current = currentTasks.filter((task) => !task.done).sort((a, b) => a.id - b.id)[0];
+  //   setCurrentTask(current);
+  //   console.log('currentTask', currentTask);
+  // }, [currentTasks])
+
+  // useEffect(() => {
+  //   if (isBreak) {
+  //     setText('Перерыв');
+  //     setNumber('');
+  //     setTime(DEFAULT_TIME_BREAK);
+  //     return;
+  //   }
+
+  //   if (currentTask && currentTask?.id !== -1) {
+  //     setText(currentTask.text);
+  //     setNumber(`${currentTasks.indexOf(currentTask) + 1}`);
+  //     setTime(currentTask.time);
+  //   } else {
+  //     setText('Введите название задачи');
+  //     setNumber('');
+  //     setTime(0);
+  //   }
+  // }, [isTimerActive]);
+
+  // useEffect(() => {
+
+  //   if (isBreak) {
+
+  //     if (time > 0) {
+  //       const currentTimerId = setTimeout(() => {
+  //         setTime(time - 1);
+  //         clearTimeout(currentTimerId);
+  //       }, 1000);
+  //     } else {
+  //       setIsBreak(false);
+  //       setIsTimerActive(false);
+  //     }
+  //   } else if (isTimerActive && currentTask?.id !== -1 ) {
+
+  //     if (time > 0) {
+  //       const currentTimerId = setTimeout(() => {
+  //         setTime(time - 1);
+  //         clearTimeout(currentTimerId);
+  //       }, 1000);
+  //     } else {
+  //       currentTask.done = true;
+  //       dispatch(authRequestAsync(data));
+  //       setIsTimerActive(false);
+  //       setIsBreak(true);
+  //     }
+  //   }
+  //   else {
+  //     setIsTimerActive(false);
+  //   }
+  // }, [isBreak, isTimerActive, time]);
 
   useEffect(() => {
     const classes = classNames(
@@ -83,11 +154,10 @@ export function PomodorBlock() {
   };
 
   function handleClickEnd() {
-    setIsTimerActive(false);
-    current.done = true;
-    dispatch(authRequestAsync(data));
-    setTime(DEFAULT_TIME_BREAK);
-    setIsTimerActive(true);
+    // currentTask.done = true;
+    // setIsTimerActive(false);
+    // dispatch(authRequestAsync(data));
+    // setIsBreak(true);
   };
 
 
--- src/shared/conts.ts
+++ src/shared/conts.ts
@@ -1,3 +1,7 @@
 // Default time 1 pomodoro
-export const DEFAULT_TIME = 25 * 60;
-export const DEFAULT_TIME_BREAK = 5 * 60;
+// export const DEFAULT_TIME = 25 * 60;
+// export const DEFAULT_TIME_BREAK = 5 * 60;
+// export const DEFAULT_TIME_BREAK_LONG = 30 * 60;
+export const DEFAULT_TIME = 10;
+export const DEFAULT_TIME_BREAK = 5;
+export const DEFAULT_TIME_BREAK_LONG = 7;
